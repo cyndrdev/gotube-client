fdfce6449b73e60073a29190d485a9233c8dc45d
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-10-10 16:59:55 +0100

    click on button to post to server

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 $(function() {
-    console.log('hello world!');
+    $("#queueAdd").click(function() {
+        console.log("clicked boi");
+
+        var url = "http://localhost:8080/queue/add";
+        var postData = JSON.stringify({
+            url: "https://www.youtube.com/watch?v=mg2cMqW_hOY"
+        });
+
+        $.post({
+            url: url, 
+            data: postData
+        })
+        .done(function () {
+            alert("success");
+        })
+        .fail(function (xhr) {
+            alert(xhr.statusText);
+        });
+
+    });
 });
