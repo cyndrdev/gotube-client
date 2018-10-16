eefcc5eaa99259e3c61ab78d1c9ed92ae8df10b6
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-10-16 18:27:01 +0100

    show search results on page

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
 $(function() {
+    $("#searchButton").click(function() {
+        var query = $("#searchQuery").val();
+
+        console.log("search: " + query); 
+
+        var url = getApiAddress() + "/search";
+        var postData = JSON.stringify({
+            query: query
+        });
+
+        $.post({
+            url: url,
+            data: postData
+        })
+        .done(function (data) {
+            // alert(data);
+
+            var results = JSON.parse(data);
+
+            var html = "<ul>";
+            for (var i = 0; i < results.length; i++) {
+                var title = results[i].title;
+                var id = results[i].id;
+
+                html += `
+                    <li>
+                        <a href="https://youtube.com/watch?v=${id}">${title}</a>
+                    </li>`;
+            }
+            html += "</ul>";
+            
+            $("#searchResults").html(html);
+        })
+        .fail(function () {
+            alert("search failed");
+        })
+        .always(function () {
+            console.log("search complete");
+        });
+    });
+
     $("#queueAdd").click(function() {
         console.log("clicked boi");
 
