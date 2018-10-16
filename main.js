4ea837a853f882fa91b5f05a58a3b09bc00645bc
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-10-16 14:50:59 +0100

    specify server address on page

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ $(function() {
     $("#queueAdd").click(function() {
         console.log("clicked boi");
 
-        var url = "http://localhost:8080/queue/add";
+        var url = getApiAddress() + "/queue/add";
         var postData = JSON.stringify({
             url: "https://www.youtube.com/watch?v=mg2cMqW_hOY"
         });
+
+        console.log("posting to:\t" + url);
 
         $.post({
             url: url, 
@@ -17,6 +19,12 @@ $(function() {
         .fail(function (xhr) {
             alert(xhr.statusText);
         });
-
     });
+
+    function getApiAddress() {
+        var address = $("#serverAddress").val();
+        var port = $("#serverPort").val();
+
+        return `http://${address}:${port}`;
+    }
 });
